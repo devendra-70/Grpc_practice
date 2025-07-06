4809a7c441ec5d3e20422be077cc02f8a9d6bce7
devendra-70 <devendramahajan70@gmail.com>
2025-07-06 20:20:20 +0530

    added logic to stramobserver

--- src/main/java/com/grpc/stock_trading_server/repository/StockRepository.java
+++ src/main/java/com/grpc/stock_trading_server/repository/StockRepository.java
@@ -4,4 +4,5 @@ import com.grpc.stock_trading_server.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockRepository extends JpaRepository<Stock,Long> {
+    Stock findByStockSymbol(String stockSymbol);
 }
--- src/main/java/com/grpc/stock_trading_server/service/StockTradingServiceImpl.java
+++ src/main/java/com/grpc/stock_trading_server/service/StockTradingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.grpc.stock_trading_server.service;
 
+import com.grpc.stock_trading_server.entity.Stock;
+import com.grpc.stock_trading_server.repository.StockRepository;
 import com.stocks.grpc.StockRequest;
 import com.stocks.grpc.StockResponse;
 import com.stocks.grpc.StockTradingServiceGrpc;
@@ -10,9 +12,30 @@ import org.springframework.stereotype.Service;
 @GrpcService
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
 
+    private final StockRepository stockRepository;
+
+    public StockTradingServiceImpl(StockRepository stockRepository){
+        this.stockRepository=stockRepository;
+    }
+
     @Override
     public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
         //find stock name -> DB -> map response -> attach to streamobserver to return
+
+        String stockSymbol=request.getStockSymbol();
+
+        Stock stock= stockRepository.findByStockSymbol(stockSymbol);
+
+        StockResponse stockResponse=StockResponse.newBuilder()
+                .setStockSymbol(stock.getStockSymbol())
+                .setPrice(stock.getPrice())
+                .setTimestamp(stock.getLastUpdated().toString())
+                .build();
+
+        responseObserver.onNext(stockResponse);
+        responseObserver.onCompleted();
+
+
 
     }
 }
